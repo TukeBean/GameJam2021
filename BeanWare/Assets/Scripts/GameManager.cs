569a278aef50682ee29b9be7797be89886f2bdc9
40177731 <rcraig18@qub.ac.uk>
2021-06-12 22:07:02 +0100

    Proper order reset on failed note + active note queue

--- BeanWare/Assets/Scripts/GameManager.cs
+++ BeanWare/Assets/Scripts/GameManager.cs
@@ -2,27 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
     public AudioSource musicTrack;
     public bool startPlaying;
     public BeatScoller beatScoller;
     public PlayerController player;
     public static GameManager instance;
+    public NoteSpawn noteSpawn;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         instance = this;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (!startPlaying)
-        {
-            if (Input.anyKeyDown)
-            {
+    void Update() {
+        if (!startPlaying) {
+            if (Input.anyKeyDown) {
                 startPlaying = true;
                 beatScoller.hasStarted = true;
                 musicTrack.Play();
@@ -30,16 +26,16 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void BeatHit()
-    {
+    public void BeatHit() {
         // this is triggered when the ingredient is successfully hit within bounds
         Debug.Log("Hit on Time");
         player.incrementCombo();
+        noteSpawn.hitNote();
     }
 
-    public void BeatMissed()
-    {
+    public void BeatMissed() {
         // this is triggered when the ingredient is NOT successfully hit within bounds or missed entirely
+        noteSpawn.failedNote();
         Debug.Log("Missed!");
     }
 }
