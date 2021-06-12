59600c8f08e597eb7fcc8b2292f5596cca5ae96d
Sam Simpson <tukesimpson@gmail.com>
2021-06-12 11:54:07 +0100

    playerObjecting

---
+++ BeanWare/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int playerHealth = 3;
+    public int enemyHealth = 8;
+    public int ComboCounter = 0;
+    public int failedOrders = 0;
+    public int successfulOrders = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void playerTakeDamage(int dmg)
+    {
+        playerHealth -= dmg;
+        // check lose condition
+        if (playerHealth == 0)
+        {
+            // LOSE DA GAME
+        }
+    }
+
+    void enemyTakeDamage(int dmg)
+    {
+        enemyHealth -= dmg;
+        // check win condition
+        if (enemyHealth == 0)
+        {
+            // WIN DA GAME - PROGRESS TO NEXT DAY
+        }
+    }
+
+    void incrementCombo()
+    {
+        ComboCounter++;
+    }
+
+    void resetCombo()
+    {
+        ComboCounter = 0;
+    }
+}
