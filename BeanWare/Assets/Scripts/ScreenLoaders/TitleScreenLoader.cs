334b8a96a4e613121d5d7b945e1348ed30c53641
amcmurray <amcmurray08@qub.ac.uk>
2021-06-13 13:00:05 +0100

    fix flashing text timing

--- BeanWare/Assets/Scripts/ScreenLoaders/TitleScreenLoader.cs
+++ BeanWare/Assets/Scripts/ScreenLoaders/TitleScreenLoader.cs
@@ -15,11 +15,25 @@ public class TitleScreenLoader : MonoBehaviour
 
     void FixedUpdate()
     {
-        TextAnimator.flashText(text);
+        flashText(text);
 
         if (Input.anyKeyDown)
         {
             Loader.Load(Loader.Scene.InstructionsScreen);
+        }
+    }
+
+    void flashText(Text text)
+    {
+        timer = timer + Time.deltaTime;
+        if (timer >= 0.5)
+        {
+            text.enabled = true;
+        }
+        if (timer >= 1)
+        {
+            text.enabled = false;
+            timer = 0;
         }
     }
 
